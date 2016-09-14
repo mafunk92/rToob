ea9263bbb58a3f4a8bd5f33ef4d4d0fdba27192b
Michael Funk <mafunk92@gmail.com>
2016-09-13 21:43:54 -0500

    Udemy Start to Lesson 30
    Used my own starter boilerplate and brought in some of his
    dependencies; will need to work on some fleshing out.

---
+++ app/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = "AIzaSyBEspt0rYOHI1v657tnRVE4wBHC9WPW7S0";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [], selectedVideo: null };
+
+        YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+            this.setState({ videos: videos, selectedVideo: videos[0] });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={ this.state.selectedVideo } />
+                <VideoList
+                    onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+                    videos={ this.state.videos }
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
