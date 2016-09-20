5f2785e3110c2870ff8aecfb01ffee852228d124
Michael Funk <mafunk92@gmail.com>
2016-09-19 19:37:14 -0500

    update for css loader

--- app/index.js
+++ app/index.js
@@ -1,28 +1,44 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
+import styles from "./assets/style.css";
+
 const API_KEY = "AIzaSyBEspt0rYOHI1v657tnRVE4wBHC9WPW7S0";
 
 class App extends Component {
+
     constructor(props) {
+
         super(props);
 
         this.state = { videos: [], selectedVideo: null };
 
-        YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+        this.videoSearch('surf')
+    }
+
+    videoSearch(term) {
+
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+
             this.setState({ videos: videos, selectedVideo: videos[0] });
         });
+
+
     }
 
     render() {
+
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={ videoSearch } />
                 <VideoDetail video={ this.state.selectedVideo } />
                 <VideoList
                     onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
     module: {
         loaders:[
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.png$/, loader: "url-loader?limit=100000" },
+            { test: /\.jpg$/, loader: "file-loader" }
         ]
     },
     output: {
